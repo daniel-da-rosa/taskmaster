8f36c20831b57b58dff4b143aae3c7725b4d461d
Daniel DR <danieldr-@hotmail.com>
2026-01-04 20:53:27 -0300

    Indentação

--- src/main/java/com/shokunin/taskmaster/src/domain/Professor.java
+++ src/main/java/com/shokunin/taskmaster/src/domain/Professor.java
@@ -9,7 +9,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Getter @Setter
+@Getter
+@Setter
 
 @ToString(exclude = "instituicoes")
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
