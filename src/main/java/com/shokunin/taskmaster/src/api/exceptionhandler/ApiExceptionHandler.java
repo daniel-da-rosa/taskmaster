6ddc85fd4ffa76729ee89c7c980ce8cb2155111e
gustavo-rosa-dex <gustavo_souza19@estudante.sesisenai.org.br>
2026-01-05 22:41:54 -0300

    Criado arquivo ApiExceptionHandler, RegraDeNegocioException para iniciar os erros de regra de negocio.

---
+++ src/main/java/com/shokunin/taskmaster/src/api/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.shokunin.taskmaster.src.api.exceptionhandler;
+
+import com.shokunin.taskmaster.src.infrastructure.exception.RegraDeNegocioException;
+import org.mapstruct.Mapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.net.URI;
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(RegraDeNegocioException.class)
+    public ProblemDetail handleRegraDeNegocio(RegraDeNegocioException e){
+
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.UNPROCESSABLE_ENTITY, e.getMessage());
+        problemDetail.setTitle("Regra de negócio");
+        problemDetail.setType(URI.create("https://shokunin.group/erros/regra-de-negocio"));
+        problemDetail.setProperty("Timestamp", LocalDateTime.now());
+
+        return problemDetail;
+    }
+}
